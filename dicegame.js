bf09094bef441feb77b890dffb37e3846a6de9ee
unknown <megdrescher@icloud.com>
2018-11-14 15:49:08 -0600

    changed functions to work more efficient

--- dicegame.js
+++ dicegame.js
@@ -1,5 +1,5 @@
 //Dice Game- Two players- Three rounds total. 
-//First player to score 100 points without passing 100 wins the game.
+//First player to score the most points within three rounds wins.
 //Round one-roll 20 and 12 sided dice. 
 //get player total for round One
 //Round two-roll 10 and 8 sided dice.
@@ -7,70 +7,83 @@
 //Last round- roll 6 and 4 sided dice.
 //get player total for round three
 //Roll a "one" on each of the dice during any of the three rounds and it's an automatic WIN.
-runDiceGame(); // not defined??
 
-function gameLogic(sides,amount) { //masterfunction 
+
+function gameLogic(sides,amount) { ///masterfunction 
 	let runDiceGame;
-	let humanPlayerOne= getUserInput("Please enter player name!");
-	let humanPlayerTwo= getUserInput("Please enter player name!");
+	let playerOne= getUserInput("Please enter player name!");
+	// let playerTwo= getUserInput("Please enter player name!");
+	let playerOneScore=0;
+
+
+	////////// ONE ROUND /////////////
+function roundOne(){
 	let dice20= rollDice(20);
 	let dice12= rollDice(12);
+	let didWinInstantly = checkForInstantWin(dice20, dice12)
+	if(didWinInstantly === true) {
+		return;
+	}
+	// if not an instant win, add to playerOne score
+
+	playerOneScore=updatePlayerScore(playerOneScore,dice20,dice12);
+
+	//////////////////////////////////
+
+function roundTwo(){
 	let dice10= rollDice(10);
 	let dice8= rollDice(8);
+	let didWinInstantly = checkForInstantWin(dice10, dice8);
+	if(didWinInstantly === true){
+		return;
+	}
+
+	playerOneScore=updatePlayerScore(playerOneScore,dice10,dice8);
+
+
+ //////////////////
+function roundThree(){
 	let dice6= rollDice(6);
 	let dice4= rollDice(4);
-	let dice1= rollDice(100);
-	let roundOneTotal= dice(20)+dice(12);
-	let roundTwoTotal= dice(10)+dice(8);
-	let roundThreeTotal = dice(6)+dice(4);
-	let finalTotal;
-	let rolledAutoWin= determineIfRolledAutoWin(dice1,dice1,roundTotal);
-	if (rolledAutoWin ===true) {
-		finalTotal= calculateAutoRollWin(dice1, dice1);
-	}
-	else {
-		finalTotal= calculateDiceRollTotal(roundTotal,dice20, dice12);
+	let didWinInstantly = checkForInstantWin(dice6, dice4);
+	if(didWinInstantly === true){
+		return;
 	}
 
-	console.log("You rolled two 1's! Autowatic Win!"+finalTotal+".");
-}
-
-
-	//let totalScore += dice10
-	//let totalScore += dice8
-function autoWin() { //autowin function call w/ parameters and a if/else 
-
-
-}
-
-
+	playerOneScore=updatePlayerScore(playerOneScore,dice6,dice4);
 	
+}
 
-function roundOne( ){
+function finalTotalScore(){
+
 
 
 }
 
-
-function rollDice(num) {
-	let roll;
-	roll=Math.floor(Math.random()*num)+1;
-	console.log(roll);
-	return roll;
+function updatePlayerScore(currentScore, roll1, roll2) {
+	let newScore=currentScore+roll1+roll2;
+	return newScore;
 }
+console.log("newScore")	
+
+
+function checkForInstantWin(roll1, roll2) {
+	if(roll1 === 1 && roll2 === 1){
+		console.log("Automatic Win!");
+		return true;
+	} else {
+		return false;
+	}
 
 
 
 
-const button = document.querySelector('button');
 
-button.onclick = function() {
-  let gameLogic = prompt('Enter Player One:');
-  alert("Player One Turn");
-}
+///document.getElementById ("Begin Game").addEventListener ("click", myFunction, false);
+
+/// myFunction() {
+	///document.getElementById("frm1").submit();
+ 
 
 
-button.onclick = function(){
-	let roll = prompt("What did you roll?");
-	alert("Roll again!");
-}
+
